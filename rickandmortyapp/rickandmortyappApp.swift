f9b8d381f60f2a10c3844957483ce5fc421a73cc
Erick Silva <ericksilva1988@gmail.com>
2023-03-21 14:59:32 -0300

    New project

---
+++ rickandmortyapp/rickandmortyappApp.swift
@@ -0,0 +1,17 @@
+//
+//  rickandmortyappApp.swift
+//  rickandmortyapp
+//
+//  Created by Erick Silva on 21/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct rickandmortyappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
